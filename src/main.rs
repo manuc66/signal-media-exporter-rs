af137967ee5a9ce460097a8ba7e33204dd10cb5e
manuc66 <manuc66@gmail.com>
2023-12-31 16:11:14 +0100

    improve error handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use rusqlite::{Connection, OpenFlags};
 use serde::{Deserialize, Serialize};
 use serde_json;
@@ -12,32 +12,29 @@ struct SignalConfig {
     key: String,
 }
 
-fn main() {
-    let signal_location = get_signal_location().unwrap();
-    let db_location = get_db_location(&signal_location).unwrap();
+fn main() -> Result<()> {
+    let signal_location = get_signal_location()?;
+    let db_location = get_db_location(&signal_location)?;
 
-    let key = get_db_key(&signal_location).unwrap();
+    let key = get_db_key(&signal_location)?;
 
-    println!("{}", db_location);
-    let conn = open_db(&db_location, key);
+    let conn = open_db(&db_location, key)?;
 
-    let mut stmt = conn
-        .prepare("select count(id) from messages where hasAttachments=1 order by id")
-        .unwrap();
-    let rows = stmt.query_map([], |row| Ok(row.get::<_, i64>(0)?)).unwrap();
+    let mut stmt =
+        conn.prepare("select count(id) from messages where hasAttachments=1 order by id")?;
+    let rows = stmt.query_row([], |row| Ok(row.get::<_, i64>(0)?))?;
 
-    for row in rows {
-        println!("Number of messages with attachments: {}", row.unwrap());
-    }
+    println!("Number of messages with attachments: {}", rows);
+
+    Ok(())
 }
 
-fn open_db(db_location: &String, key: String) -> Connection {
+fn open_db(db_location: &String, key: String) -> Result<Connection> {
     let flags = OpenFlags::SQLITE_OPEN_READ_ONLY;
-    let connection = Connection::open_with_flags(db_location.clone(), flags).unwrap();
-    connection
-        .pragma_update(None, "key", format!("x'{}'", key))
-        .unwrap();
-    connection
+    let connection = Connection::open_with_flags(db_location.clone(), flags)?;
+    connection.pragma_update(None, "key", format!("x'{}'", key))?;
+
+    Ok(connection)
 }
 
 fn get_db_key(signal_location: &String) -> Result<String> {
@@ -56,44 +53,47 @@ fn get_db_key(signal_location: &String) -> Result<String> {
     Ok(data.key)
 }
 
-fn get_db_location(signal_location: &String) -> Result<String, String> {
+fn get_db_location(signal_location: &String) -> Result<String> {
     let db_path = PathBuf::from(signal_location).join("sql/db.sqlite");
 
-    db_path.to_str().map(|x| x.to_owned()).ok_or(format!(
-        "Path is no a valid utf8 string: {}",
-        db_path.display()
-    ))
+    db_path
+        .to_str()
+        .map(|x| x.to_owned())
+        .ok_or(anyhow!(format!(
+            "Path is no a valid utf8 string: {}",
+            db_path.display()
+        )))
 }
 
-fn get_signal_location() -> Result<String, String> {
+fn get_signal_location() -> Result<String> {
     let os = env::consts::OS;
     let signal_path = PathBuf::from(get_user_profile_folder()?)
         .join(match os {
             "windows" => "AppData/Roaming",
             "linux" => ".config",
             "macos" => "Library/Application Support",
-            _ => return Err(format!("Unsupported OS: {}", os)),
+            _ => return Err(anyhow!("Unsupported OS: {}", os)),
         })
         .join("Signal/");
 
     Ok(signal_path
         .to_str()
-        .ok_or(format!(
+        .ok_or(anyhow!(
             "Path is no a valid utf8 string: {}",
             signal_path.display()
         ))?
         .to_owned())
 }
 
-fn get_user_profile_folder() -> Result<String, String> {
+fn get_user_profile_folder() -> Result<String> {
     let os = env::consts::OS;
     match os {
-        "windows" => read_env("USERPROFILE"),
-        "macos" | "linux" => read_env("HOME"),
-        _ => Err(format!("Unsupported os: {}", os)),
+        "windows" => Ok(read_env("USERPROFILE")?),
+        "macos" | "linux" => Ok(read_env("HOME")?),
+        _ => Err(anyhow!("Unsupported os: {}", os)),
     }
 }
 
-fn read_env(var: &str) -> Result<String, String> {
-    env::var(var).map_err(|e| format!("Error while reading {}: {}", var, e))
+fn read_env(var: &str) -> Result<String> {
+    env::var(var).map_err(|e| anyhow!("Error while reading {}: {}", var, e))
 }
